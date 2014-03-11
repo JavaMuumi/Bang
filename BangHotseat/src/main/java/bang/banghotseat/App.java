9dc74e5e4e8e76a8a321d3768df4db067a425f81
JavaMuumi <antti.korpi@helsinki.fi>
2014-03-11 18:12:28 +0200

    NetBeans sovitus

---
+++ BangHotseat/src/main/java/bang/banghotseat/App.java
@@ -0,0 +1,13 @@
+package bang.banghotseat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
